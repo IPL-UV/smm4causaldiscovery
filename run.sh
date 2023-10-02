35553c74b63fcc76a7da82d2b0b6bbb7136e8b13
gherardo varando <gherardo.varando@gmail.com>
2023-10-02 11:40:03 +0200

    main run file

---
+++ run.sh
@@ -0,0 +1,14 @@
+### run experiments
+python3 run_experiments.py --generated1 --benchmarks --mixgenerated --tuebingen2 
+
+
+## plot stuff 
+Rscript plot_results.R  
+
+Rscript plot_benchmarks.R 
+
+Rscript plot_df.R
+
+Rscript plot_mixgenerated.R  
+
+Rscript plot_tub.R
